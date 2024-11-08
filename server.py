a1ce6f8b1e199cc4341da38935c02bcaeb382688
Soggy Bread <diegovan0616@gmail.com>
2024-11-07 22:27:02 -0500

    Implement basic sever functions

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 import socket
 import sys
 
-
 #Create a Socket (connect two computers)
 def create_socket():
     try:
@@ -25,9 +24,38 @@ def bind_socket():
 
         print("Binding port #" + str(port))
     
+#FIX THIS: TUTORIAL GIVES 2 ARGUMENTS, SHIT DON'T WORK!!!!!!
         s.bind(host, port)
         s.listen(5)
 
     except socket.error as msg:
             print("Socket Binding error" + str(msg) + "\n" + "Retrying...")
             bind_socket()
+
+#Establish connection with a client (socket must be listening)
+def socket_accept():
+    conn, address = s.accept()
+    print("Connection has been established! |" + " IP: " + address[0] + " | Port" + str(address[1]))
+
+    send_commands(conn)
+    conn.close()
+
+#Send commands to client
+def send_commands(conn):
+    while True:
+        cmd = input()
+        if cmd == 'quit':
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024), "utf-8")
+            print(client_response, end ="")
+
+def main():
+    create_socket()
+    bind_socket()
+    socket_accept()
+
+main()
